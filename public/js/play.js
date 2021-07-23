192f46f194ddc561811fb26e9a7d74eba4b57495
Flora Huang <florahuang221@gmail.com>
2021-07-23 21:46:07 +0000

    began the play part of the game

--- public/js/play.js
+++ public/js/play.js
@@ -1,4 +1,38 @@
 //get a random response
+window.onload = function(){
+    getResponse();
+}
+
+
+const getResponse = () => {
+    const messageRef = firebase.database().ref();
+
+    messageRef.on("value", (snapshot) => {
+        const data = snapshot.val();
+        console.log(data);
+
+        let size = 0;
+
+        for(const recordKey in data){
+            size++;
+        }
+        console.log(size);
+
+        //gives us our rando number :)
+        let randomval = getRandomInt(size);
+        console.log(randomval);
+
+        
+        const record = data[randomval];
+        console.log(record);
+
+    });
+
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
 
 //check if guess is correct (if matches colour card green)
 
